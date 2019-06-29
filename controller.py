0b4036dd91e3d83f2f647d13928cffe22aff1ab2
Rebecca <rebeccachoosir@gmail.com>
2019-06-30 01:43:29 +0900

    chatroom 코드 작성

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import sqlite3
+import datetime
 
 app = Flask(__name__)
 
@@ -12,9 +13,9 @@ def auth():
     
         #join: addUser
         if request.method == 'POST':
-            email = request.form['email']
-            nickname = request.form['nickname']
-            profilepic = request.form['profilepic']
+            email = request.form['user_email']
+            nickname = request.form['user_nickname']
+            profilepic = request.form['user_profilepic']
             c.execute("INSERT INTO User(email, nickname, profilepic) VALUES (?,?,?)", [email, nickname, profilepic])
             conn.commit()
             return "join success"
@@ -22,8 +23,8 @@ def auth():
 
         #login: getUser
         elif request.method == 'GET':
-            email = request.form['email']
-            c.execute("SELECT * FROM User WHERE email = '{}'".format(email))
+            user_email = request.form['user_email']
+            c.execute("SELECT * FROM User WHERE email = '{}'".format(user_email))
             user_info = c.fetchall()        
             return "login success"
     
@@ -39,6 +40,66 @@ def auth():
 
 @app.route("/chatRoom", methods=["GET", "POST", "PUT", "DELETE"])
 def chatRoom():
+    try:
+        conn = sqlite3.connect("/root/10s.db")
+        c = conn.cursor()
+
+        #create a chatroom
+        if request.method == 'POST':
+            user_id = request.form['user_id']
+            roomname = request.form['room_name']
+            time_created = datetime.datetime.now()
+            c.execute("INSERT INTO ChatRoom(RoomName, CreateDate) VALUES (?,?)", [roomname, time_created])
+            conn.commit()
+
+            c.execute("SELECT ID FROM ChatRoom WHERE RoomName = roomname")
+            room_id = c.fetchone()
+            
+            c.execute("INSERT INTO ChatUser(RoomID, UserID) VALUES (?,?)", [room_id,user_id])
+            conn.commit()
+
+            return "chatroom created"
+
+        #get chatroom that user is in
+        elif request.method == 'GET':
+            user_id = request.form[user_id]
+            c.execute("SELECT RoomID FROM ChatUser WHERE UserID = user_id")
+            room_id = c.fetchall()
+
+            return "chatroom fetched"
+
+
+        #delete chatroom
+        elif request.method == 'DELETE':
+            room_id = request.form[room_id]
+            c.execute("DELETE FROM ChatRoom WHERE ID = ?", (room_id,))
+            conn.commit()
+
+            c.execute("DELETE FROM ChatUser WHERE RoomID = ?", (room_id,))
+            conn.commit()
+
+            return "chatroom deleted"
+
+
+        #invite friend to chatroom
+        elif request.method == 'PUT':
+            room_id = request.form[room_id]
+            user_id = request.form[user_id]
+            c.execute("INSERT INTO ChatUser(RoomID, UserID) VALUES(?,?)", [room_id, user_id])
+            conn.commit()
+
+            return "friend invited"
+
+
+
+
+    except:
+        print("log")
+        conn.rollback()
+
+    finally:
+        conn.close()
+
     return "chat room"
 
 

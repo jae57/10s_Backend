1dea36f524191ec193780dd4af72167ce90c75b8
changmin-dev <changmin043@gmail.com>
2019-06-29 21:53:45 +0900

    [Fix] 로그인 타입 추가

--- user.py
+++ user.py
@@ -1,8 +1,9 @@
 class User:
-    def __init__(self, id, email, nickname, status, profile_image, token):
+    def __init__(self, id, email, nickname, status, profile_image, auth_token, auth_type):
         self.id = id
         self.email = email
         self.nickname = nickname # 닉네임은 기본은 원래 이름을 사용한다.
-        self.status = status
+        self.status = status # 상태 - 자기 소개 같은거
         self.profile_image = profile_image
-        self.token = token
+        self.auto_token = token
+        self.auth_type = auth_type

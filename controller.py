30777c2db36f43caccb3ace8d0d179c8cb09d015
changmin-dev <changmin043@gmail.com>
2019-07-14 17:46:29 +0900

    [refactor] 데이터 계층 분리

--- controller.py
+++ controller.py
@@ -2,306 +2,166 @@
 from flask import Flask, request, jsonify, json, g
 import sqlite3
 import datetime
-import sys
 import message_manager
 import s3_manager
 import logging
+import db_manager
 
 app = Flask(__name__)
 message_manager = message_manager.MessageManager()
-
-chatroom = dict()
+db_manager = db_manager.DatabseManager("10s.db")
 
 def json_message(message):
     return jsonify({"message": message})
 
+
 @app.route("/api/auth", methods=["GET", "POST"])
 def auth():
-    print("auth")
-    try:
-        conn = sqlite3.connect("10s.db")
-        print("conn")
-        c = conn.cursor()
-        print("cursor")
-        # join: addUser
-        if request.method == 'POST':
-            body = request.json
-            email = body["email"]
-            token = hash(email)
-            auth_token = {'auth_token': token}
-            nickname = body['nickname']
-            profile_image = body['profile_image']
-            c.execute(
-                "INSERT INTO user(email, nickname, profile_image, modified_date, status_message, auth_token) VALUES (?,?,?,?,?,?)",
-                [email, nickname, profile_image, datetime.datetime.now(), "", token])
-            conn.commit()
-            return jsonify(auth_token), 200
+    # join: addUser
+    if request.method == 'POST':
+        body = request.json
+        email = body["email"]
+        nickname = body['nickname']
+        profile_image = body['profile_image']
 
+        token = hash(email)
+        db_manager.add_user(email, nickname, profile_image, token)
 
-        # login: getUser
-        elif request.method == 'GET':
-            user_email = request.headers['email']
-            c.execute("SELECT auth_token FROM user WHERE email = '{}'".format(user_email))
-            auth_token = c.fetchone()
-            if auth_token == None:
-                return jsonify(message="Not Exist User"), 400
+        auth_token = {'auth_token': token}
+        return jsonify(auth_token), 200
 
-            result_json = {'auth_token': auth_token[0]}
-            return jsonify(result_json), 200
+    # login: getUser
+    else: #request.method == 'GET':
+        user_email = request.headers['email']
+        auth_token = check_user(user_email)
+        if auth_token == None:
+            return jsonify(message="Not Exist User"), 400
 
-    except TypeError:
-        conn.rollback()
-        raise
-    except:
-        conn.rollback()
-        print(sys.exc_info()[0])
-        raise
-
-    finally:
-        conn.close()
-
-    return jsonify(message="ERROR"), 500
-
-
+        result_json = {'auth_token': auth_token[0]}
+        return jsonify(result_json), 200
+    
 
 @app.route("/api/chatRoom", methods=["GET", "POST", "PUT", "DELETE"])
 def chat_room():
-    try:
-        conn = sqlite3.connect("10s.db")
-        c = conn.cursor()
+    #try:
+    # create a chat_room
+    if request.method == 'POST':
+        auth_token = request.headers["Authorization"].split()[1]
+        user_id = db_manager.get_user_id(auth_token)
 
-        # create a chat_room
-        if request.method == 'POST':
-            auth_token = request.headers["Authorization"].split()[1]
-            c.execute("SELECT id FROM user WHERE auth_token = ?",[auth_token])
-            user_id = c.fetchone()[0]
-            body = request.json
-            room_name = body['room_name']
-            time_created = datetime.datetime.now()
-            c.execute("INSERT INTO chat_room(room_name, create_date) VALUES (?,?)", [room_name, time_created])
-            conn.commit()
+        body = request.json
+        room_name = body['room_name']
+        time_created = datetime.datetime.now()
 
-            c.execute("SELECT id FROM chat_room WHERE room_name = ?", [room_name])
-            room_id = c.fetchone()[0]
+        db_manager.create_chatroom(room_name, time_created, user_id)
 
-            c.execute("INSERT INTO chat_user(room_id, user_id) VALUES (?,?)", [room_id, user_id])
-            conn.commit()
+        return json_message("chat_room created"), 200
 
-            return json_message("chat_room created"), 200
+    ## get chat_room that user is in
+    elif request.method == 'GET':
+        auth_token = request.headers["Authorization"].split()[1]
+        user_id = db_manager.get_user_id(auth_token)
+        
+        room_infos = db_manager.get_room_infos(user_id)
 
-        ## get chat_room that user is in
-        elif request.method == 'GET':
-            auth_token = request.headers["Authorization"].split()[1]
-            c.execute("SELECT id FROM user WHERE auth_token = '"+auth_token+"'")
-            user_id = c.fetchone()[0]
-            c.execute("SELECT cr.id, cr.room_name FROM chat_user AS cu INNER JOIN chat_room AS cr on cu.room_id = cr.id WHERE cu.user_id="+str(user_id))
-            room_infos = c.fetchall()
-            room_infos_json = list()
-            for room_info in room_infos:
-                room_infos_json.append({"room_id":room_info[0], "room_name":room_info[1]})
-            return jsonify(room_infos_json), 200
+        room_infos_json = list()
+        
+        for room_info in room_infos:
+            room_infos_json.append({"room_id":room_info[0], "room_name":room_info[1]})
+        
+        return jsonify(room_infos_json), 200
 
-        # delete chat_room
-        elif request.method == 'DELETE':
-            body = request.json
-            room_id = body['room_id']
-            auth_token = request.headers["Authorization"].split()[1]
-            c.execute("SELECT id FROM user WHERE auth_token = '"+auth_token+"'")
-            user_id = c.fetchone()[0]
+    # delete chat_room
+    elif request.method == 'DELETE':
+        body = request.json
+        room_id = body['room_id']
+        auth_token = request.headers["Authorization"].split()[1]
+        user_id = db_manager.get_user_id(auth_token)
+        
+        db_manager.out_chatroom(room_id, user_id)
 
-            c.execute("DELETE FROM chat_user WHERE room_id = ? AND user_id=?", [room_id,user_id])
-            conn.commit()
+        check_delete_room(room_id)
 
-            c.execute("SELECT count(*) FROM chat_user WHERE room_id = "+str(room_id))
-            remains = c.fetchone()[0]
-            if remains == 0:
-                c.execute("DELETE FROM chat_room WHERE id = ?", (room_id,))
-                conn.commit()
-
-            return json_message("exit successfully"), 200
+        return json_message("exit successfully"), 200
 
 
-        # invite friend to chat_room
-        elif request.method == 'PUT':
-            body = request.json
-            room_id = body['room_id']
-            invited_id = body['invited_id']
-            c.execute("INSERT INTO chat_user(room_id, user_id) VALUES(?,?)", [room_id, invited_id])
-            conn.commit()
+    # invite friend to chat_room
+    elif request.method == 'PUT':
+        body = request.json
+        room_id = body['room_id']
+        invited_id = body['invited_id']
+        db_manager.update_chatuser(room_id, invited_id)
 
-            return json_message("friend invited"), 200
-
-
-    except:
-        print(sys.exc_info()[0])
-        conn.rollback()
-
-    finally:
-        conn.close()
-
-    return jsonify(message="ERROR"), 500
+        return json_message("friend invited"), 200
 
 
 @app.route("/api/friend", methods=["GET", "POST"])
 def friend():
-    try:
-        conn = sqlite3.connect("10s.db")
-        c = conn.cursor()
-        # search friend
-        if request.method == 'GET':
-            auth_token = request.headers["Authorization"].split()[1]
-            c.execute("SELECT id FROM user WHERE auth_token ='"+auth_token+"'")
-            user_id = c.fetchone()[0]
-            c.execute(
-                "SELECT id, nickname, profile_image, status_message FROM user INNER JOIN friend ON friend.friend_id = user.id WHERE user_id = ?",
-                [user_id])
-            friend_info = c.fetchall()
-            friends = []
-            for friend in friend_info:
-                friend = {'id': friend[0], 'nickname': friend[1], 'profile_image': friend[2], 'status_message': friend[3]}
-                friends.append(friend)
+    auth_token = request.headers["Authorization"].split()[1]
+    user_id = db_manager.get_user_id(auth_token)
 
-            dic = { 'friends' : friends }
-            return json.dumps(dic, ensure_ascii=False), 200
+    # search friend
+    if request.method == 'GET':
+        friend_infos = db_manager.get_friend_info(user_id)
+        friends = []
+        for friend_info in friend_infos:
+            friend = {'id': friend_info[0], 'nickname': friend_info[1], 'profile_image': friend_info[2], 'status_message': friend_info[3]}
+            friends.append(friend)
 
-        # add friend
-        elif request.method == 'POST':
-            auth_token = request.headers['Authorization'].split()[1]
-            c.execute("SELECT id FROM user WHERE auth_token = '"+auth_token+"'")
-            user_id = c.fetchone()[0]
-            body = request.json
-            friend_email = body['friend_email']
-
-            c.execute("SELECT id FROM user WHERE email='{}'".format(friend_email))
-            friend_id = c.fetchone()
-
-            if friend_id is None:
-                return jsonify({"message": "cannot find friend"}), 400
-            else:
-                friend_id = friend_id[0]
-            c.execute("INSERT INTO friend(user_id, friend_id) VALUES(?,?)", [user_id, friend_id])
-            c.execute("INSERT INTO friend(user_id, friend_id) VALUES(?,?)", [friend_id, user_id])
-            conn.commit()
-            return json_message("add friend success"), 200
-
-    except sqlite3.OperationalError:
-        conn.rollback()
-        raise
-    except sqlite3.InterfaceError:
-        conn.rollback()
-        raise
-    except:
-        print(sys.exc_info()[0])
-        conn.rollback()
-    finally:
-        conn.close()
-
-    return jsonify(message="ERROR"), 500
+        dic = { 'friends' : friends }
+        return json.dumps(dic, ensure_ascii=False), 200
 
 
 @app.route("/api/profile/<user_id>", methods=["GET"])
-def friend_profile(user_id):
-    try:
-        conn = sqlite3.connect("10s.db")
-        c = conn.cursor()
+def get_profile(user_id):
+    user_profile = get_user_profile(user_id)
+    return json.dumps(user_profile, ensure_ascii=False)
 
-        # get friend profile
-        if request.method == 'GET':
-            c.execute("SELECT * FROM user where id=" + user_id)
-            row = c.fetchone()
-            user = {'nickname': row[2], 'status_message': row[5], 'profile_image': row[3]}
-            return json.dumps(user, ensure_ascii=False)
-
-    except KeyError:
-        conn.rollback()
-        raise
-
-    except:
-        print(sys.exc_info()[0])
-        conn.rollback()
-
-    finally:
-        conn.close()
-
-    return jsonify(message="ERROR"), 500
 
 @app.route("/api/profile", methods=["GET", "PUT"])
 def profile():
-    try:
-        conn = sqlite3.connect("10s.db")
-        c = conn.cursor()
+    auth_token = request.headers["Authorization"].split()[1]
+    user_id = db_manager.get_user_id(auth_token)
+    
+    # get profile
+    if request.method == 'GET':
+        # 내 프로필 정보
+        return get_profile(user_id)
 
-        # get profile
-        if request.method == 'GET':
-            # 내 프로필 정보
-            auth_token = request.headers['Authorization'].split()[1]
-            c.execute("SELECT id FROM user WHERE auth_token = '" + auth_token + "'")
-            user_id = c.fetchone()[0]
-            c.execute("SELECT * FROM user where id=" + str(user_id))
-            row = c.fetchone()
-            user = {'nickname': row[2], 'status_message': row[4], 'profile_image': row[3]}
-            return json.dumps(user, ensure_ascii=False)
-
-        # update profile
-        elif request.method == 'PUT':
-            auth_token = request.headers['Authorization'].split()[1]
-            c.execute("SELECT id FROM user WHERE auth_token = '" + auth_token + "'")
-            user_id = c.fetchone()[0]
-            
-            new_nickname = request.form['nickname']
-            new_status = request.form['status_message']
-            image_file = request.files['profile_image']
-            new_image = s3_manager.upload_file(image_file, user_id, "10s-profile", image_file.filename)
-            new_image = image_file.filename
-            c.execute("UPDATE user SET nickname = '" + new_nickname +
-                      "', status_message = '" + new_status +
-                      "', profile_image= '" + new_image +
-                      "', modified_date = datetime('now') where id=" + str(user_id)
-                      );    
-            conn.commit()
-            return json_message("user updated"), 200
-
-    except KeyError:
-        conn.rollback()
-        raise
-
-    except:
-        print(sys.exc_info()[0])
-        conn.rollback()
-
-    finally:
-        conn.close()
-
-    return jsonify(message="ERROR"), 500
+    # update profile
+    elif request.method == 'PUT':
+        new_nickname = request.form['nickname']
+        new_status = request.form['status_message']
+        image_file = request.files['profile_image']
+        new_image_path = s3_manager.upload_file(image_file, user_id, "10s-profile", image_file.filename)
+        db_manager.update_user_profile(new_nickname, new_status, new_image_path, user_id)
+        
+        return json_message("user updated"), 200
 
 
 #when message is received from client
 @app.route("/api/chatRoom/<int:id>/message", methods = ['POST', 'GET'])
 def receive(id):
-        if request.method == 'POST':
-                conn = sqlite3.connect("10s.db")
-                c = conn.cursor()
+    if request.method == 'POST':
+        auth_token = request.headers["Authorization"].split()[1]
+        user_id = get_user_id(auth_token)
 
-                #upload file onto S3
-                f = request.files['file']
-                
-                path = s3_manager.upload_file(f, id, '10s-voice', f.filename)
+        #upload file onto S3
+        f = request.files['file']
+        path = s3_manager.upload_file(f, id, '10s-voice', f.filename)
 
-                auth_token = request.headers["Authorization"].split()[1]
-                c.execute("SELECT `id` FROM `user` WHERE `auth_token` = ?", [auth_token])
-                user_id = c.fetchone()
+        order = message_manager.countMessage(id) + 1
+        message = {"index" : order, 
+                                "sender" : user_id,
+                                "receiver" : id, 
+                                "content" : path,
+                                "date" : datetime.datetime.now()}
+        message_manager.pushMessage(id, message)
+        return json_message("success"), 200
 
-                order = message_manager.countMessage(id) + 1
-                message = {"index" : order, 
-                                        "sender" : user_id,
-                                        "receiver" : id, 
-                                        "content" : path,
-                                        "date" : datetime.datetime.now()}
-                message_manager.pushMessage(id, message)
-                return json_message("success"), 200
-        else:
-            result = message_manager.getMessage(id)
-            return jsonify({"messages": result}), 200
+    else:
+        result = message_manager.getMessage(id)
+        return jsonify({"messages": result}), 200
 
 
 #bring messages with index
